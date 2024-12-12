6d89383fdfa51678241eec09b011ad31c270ebee
Bassi-sher4396 <hardikbassi230@gmail.com>
2024-12-12 13:28:33 +0000

    stack and heap memory

---
+++ 01_basics/06_stack_and_heap_memory.js
@@ -0,0 +1,16 @@
+//stack(primitive) and heap(non-primitive)
+let myYotubename = "fuddi"
+let anothername =myYotubename
+anothername= "fuddi_ch_lun"
+console.log(myYotubename);
+
+console.log(anothername);
+let userOne = {
+    email: "kjfv",
+    upi: "bb"
+
+}
+let userTwo = userOne
+userTwo.email="ll"
+console.log(userOne)
+console.log(userTwo);
