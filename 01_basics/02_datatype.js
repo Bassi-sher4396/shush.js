61556e98950cdec20a0432d3300242362f05bdc8
Bassi-sher4396 <hardikbassi230@gmail.com>
2024-12-12 11:09:30 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict" //treat all js as newer version
+//alert(3+3) //we are using node js not browser
+console.log(3+3) //aagar yhin pe mai likh du ek aur console without semi colon after the previous console then it would be an error ya to line alag krke likhna pdega (if without semi colon)
+let name =" se"
+let age = 12
+let isLOGGESIn = false
+let state = null
+//string => ""
+//  bigint boolean number
+//null=>standalone value
+//undefined=>value not assigned
+//symbol => unique
+
+//object
+console.log(typeof null)//null ka type object ata hai
