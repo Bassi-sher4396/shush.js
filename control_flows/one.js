ed2b62dfe983a2d7a042b8500e035244420c3cec
Bassi-sher4396 <hardikbassi230@gmail.com>
2024-12-17 09:19:38 +0000

    if else switch

---
+++ control_flows/one.js
@@ -0,0 +1,18 @@
+//if
+// 
+const balance =1000
+if(balance>500) console.log(`fudda maaro`); //multiple lines can also be written without curly parenthesis but thats nt a good practice
+
+const month = 8
+
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+
+    default:
+        console.log(`default`);
+        
+        break;
+}
